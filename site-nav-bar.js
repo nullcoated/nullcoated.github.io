fe3185c5e9e37c4d32cab2555963bd94dfe244a7
vaastab <vaastab@noreply.codeberg.org>
2022-09-19 19:54:47 +0530

    add nav bar scrpt

---
+++ site-nav-bar.js
@@ -0,0 +1,9 @@
+document.write('<nav id="site-nav-bar">\
+        <a href="index.html">Home</a>\
+        <a href="writeups-main.html">WriteUps</a>\
+        <a href="blogs-main.html">Blogs</a>\
+        <a href="#">EMYJEMS</a>\
+        <a href="projects-main.html">Projects</a>\
+        <a href="guides-main.html">Guides</a>\
+        <a href="about.html">About</a>\
+    </nav>');
