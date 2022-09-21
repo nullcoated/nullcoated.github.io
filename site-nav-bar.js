7f29e85ebbcbadfa637ad1ef19d3c912e5c82d8a
vaastab <vaastab@noreply.codeberg.org>
2022-09-21 16:25:26 +0530

    uttered

--- site-nav-bar.js
+++
@@ -1,9 +0,0 @@
-document.write('<nav id="site-nav-bar">\
-        <a href="index.html">Home</a>\
-        <a href="writeups-main.html">WriteUps</a>\
-        <a href="blogs-main.html">Blogs</a>\
-        <a href="#">EMYJEMS</a>\
-        <a href="projects-main.html">Projects</a>\
-        <a href="guides-main.html">Guides</a>\
-        <a href="about.html">About</a>\
-    </nav>');
