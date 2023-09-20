12b2bf7bc87e3ddd9c279bc4fd3ff5c6cefcb40c
vaastab <vaastab@uknownmail.none>
2023-09-20 15:48:07 +0530

    add site contents

---
+++ site-nav-bar.js
@@ -0,0 +1,6 @@
+document.write('<nav id="site-nav-bar">\
+        <a href="index-main.html">Home</a>\
+        <a href="blogs-main.html">Blogs</a>\
+        <a href="guides-main.html">Guides</a>\
+        <a href="about.html">About</a>\
+    </nav>');
