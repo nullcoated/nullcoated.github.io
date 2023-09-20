f08410c91990cbc3493490af7fbc277f4e27ad88
vaastab <vaastab@uknownmail.none>
2023-09-20 15:55:50 +0530

    little fix

--- site-nav-bar.js
+++ site-nav-bar.js
@@ -1,5 +1,5 @@
 document.write('<nav id="site-nav-bar">\
-        <a href="index-main.html">Home</a>\
+        <a href="index.html">Home</a>\
         <a href="blogs-main.html">Blogs</a>\
         <a href="guides-main.html">Guides</a>\
         <a href="about.html">About</a>\
