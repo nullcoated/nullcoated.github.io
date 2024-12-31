35dc71d1ca81908fedd1824558deca5e0b85cf9b
nullcoated <112262602+nullcoated@users.noreply.github.com>
2024-12-31 21:11:04 +0530

    Update site-nav-bar.js

--- site-nav-bar.js
+++ site-nav-bar.js
@@ -1,6 +1,6 @@
 document.write('<nav id="site-nav-bar">\
         <a href="index.html">Home</a>\
         <a href="blogs-main.html">Blogs</a>\
-        <a href="guides-main.html">Guides</a>\
+        <a href="friends.html">Friends</a>\
         <a href="about.html">About</a>\
     </nav>');
